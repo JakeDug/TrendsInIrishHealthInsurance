92d2309639c3d31a1bdfe6fe690e69dd7cbb72af
Jake Duggan <c00221072@itcarlow.ie>
2018-10-12 11:01:07 +0100

    Changed variable names

--- downloadFile.py
+++ downloadFile.py
@@ -1,10 +1,8 @@
 import requests
 
 def downloadFile(downloadUrl):
-	resp = requests.get(downloadUrl)
+	response = requests.get(downloadUrl)
 	output = open('C:/Users/Jake/OneDrive/4th Year/project/dataFiles/tests.xlsx', 'wb')
-	output.write(resp.content)
+	output.write(response.content)
 	print(downloadUrl + " downloaded")
 	output.close()
-
-downloadFile("https://www.hia.ie/sites/default/files/Vhi%20price%20template%2001.07.2018.xlsx")
