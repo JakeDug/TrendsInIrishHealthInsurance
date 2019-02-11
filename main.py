199b897818654c397fd060f3bd4e7c99f66c0b96
JakeDug <c00221072@itcarlow.ie>
2019-02-11 17:28:59 +0000

    Implemented flask gui

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session, flash
+from graph import createPlotGraph
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', the_title='HIPAS')
+
+if __name__ == '__main__':
+    app.secret_key = 'thebiglebowski;'
+    app.run(debug=True)
+
