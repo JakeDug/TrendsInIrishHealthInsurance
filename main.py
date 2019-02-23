18b3e223395028d0c154ca135245f31b578c8219
Jake Duggan <c00221072@itcarlow.ie>
2019-02-23 13:51:57 +0000

    Added Database connection

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, request, session, flash
 from graph import createPlotGraph
 import sys
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#Database Connection
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="hipas",
+    password="sy42azyc",
+    hostname="hipas.mysql.pythonanywhere-services.com",
+    databasename="hipas$default",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
