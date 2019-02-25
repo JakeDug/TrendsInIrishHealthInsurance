234616662d33885463948b520a235a017615b419
Jake Duggan <c00221072@itcarlow.ie>
2019-02-25 16:10:31 +0000

    added insuranceData class to use with sqlalchamy

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from graph import createPlotGraph
 import sys
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
 
 #Database Connection
 
@@ -19,9 +21,33 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+#define InsuranceData table + data
+class InsuranceData(db.Model):
+
+	__tablename__ = "insuranceData"
+	plan_name = db.Column(db.String(40), primary_key=True)
+	adult = (db.Float)
+	young_adult_age_25 = db.Column(db.Float)
+	young_adult_age_24 = db.Column(db.Float)
+	young_adult_age_23 = db.Column(db.Float)
+	young_adult_age_22 = db.Column(db.Float)
+	young_adult_age_21 = db.Column(db.Float)
+	young_adult_age_20 = db.Column(db.Float)
+	young_adult_age_19 = db.Column(db.Float)
+	young_adult_age_18 = db.Column(db.Float)
+	child_one = db.Column(db.Float)
+	child_two = db.Column(db.Float)
+	child_three = db.Column(db.Float)
+	child_four = db.Column(db.Float)
+	newborn = db.Column(db.Float)
+	date = db.Column(db.DateTime, nullable=False, primary_key=True)
+
+
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html', the_title='HIPAS')
+	return render_template('index.html', the_title='HIPAS', insuranceData = InsuranceData.query.all())
 
 if __name__ == '__main__':
     app.secret_key = 'thebiglebowski;'
